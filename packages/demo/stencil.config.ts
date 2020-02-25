6de3e6441b87b47c5a07f373dba18f89846f1dbc
Nate Moore <nate@natemoo.re>
2020-02-24 21:47:58 -0600

    add styles to demo

--- packages/demo/stencil.config.ts
+++ packages/demo/stencil.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  globalStyle: 'src/global/index.css',
   outputTargets: [
     {
       type: 'www',
